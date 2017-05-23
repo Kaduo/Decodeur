cd666651e8c169f0e0999af250c72dba08f9fe77
Antoine He <Antoine.He@grenoble-inp.org>
2017-05-23 15:53:35 +0200

    Zigzag inverse

--- include/reconstruction.h
+++ include/reconstruction.h
@@ -6,12 +6,33 @@
 #include "bitstream.h"
 #include "extraction.h"
 
+/* Orientation dans le zigzag */
+enum orientation {
+  ORI_E = 0, // Vers la droite
+  ORI_S = 1, // Vers le bas
+  ORI_NE = 2, // Vers le haut droit
+  ORI_SW = 3, // Vers le bas gauche
+  /* Sentinelle */
+  ORI_NB
+};
+
+/* Structure de point ordonné */
+struct point {
+  int8_t x;
+  int8_t y;
+};
 
 /* Quantification inverse : multiplie la composante par la table de quantification donnée */
 extern void inverse_quant(int16_t *component, uint8_t *quant_table);
 
-/* Zag zig la composante */
+/* Zig-zag inverse de la composante */
 extern void zag_zig(int16_t *component);
+
+/* Met à jour le point courant */
+extern void update_point(struct point *point, enum orientation orientation);
+
+/* Renvoie l'orientation suivante */
+enum orientation next_orientation(enum orientation ori, const struct point *point);
 
 /* Applique la transformée en cosinus discrète inverse sur la composante */
 extern void idct(int16_t *component);
