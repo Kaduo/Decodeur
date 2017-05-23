1c4d8830068837061fd556eba4551cbeca098f04
Antoine He <Antoine.He@grenoble-inp.org>
2017-05-23 10:14:17 +0200

    Bouchon ppm.c

--- include/ppm.h
+++ include/ppm.h
@@ -2,10 +2,28 @@
 #define __PPM_H__
 
 #include <stdint.h>
-#include <stdbool.h>
-#include "bitstream.h"
+#include <stdlib.h>
+#include "upsampling.h"
 
-/*Convertit un tableau de pixels en un ppm.*/
-void pixels_to_ppm(const struct *pixel_rgb pixels);
+/* Cree un fichier PPM a partir d'une image (tableau uint32_t) */
+extern void create_image_ppm(const struct block *blocks,
+                        uint32_t width,
+                        uint32_t height,
+                        char *filename);
+
+/* Ecrit l'entete d'un fichier PPM */
+extern void write_ppm_header(uint32_t width, uint32_t height, char *filename);
+
+/* Ecrit les données d'un fichier PPM */
+extern void write_ppm_data(const struct block *blocks,
+                      uint32_t width,
+                      uint32_t height,
+                      char *filename);
+
+/* Ouvre un fichier dans un mode donne */
+extern void open_file(FILE **file, char *filename, char *mode);
+
+/* Ferme un fichier */
+extern void close_file(FILE **file, char *filename);
 
 #endif
