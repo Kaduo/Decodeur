33a24bac49f5b9e04ef3208c88404cd707bc49bd
Mathieu Barbe <mat.barbe@gmail.com>
2017-05-31 15:29:27 +0200

    ajout de la création de test.jpeg dans le main

--- src/jpeg2ppm.c
+++ src/jpeg2ppm.c
@@ -6,7 +6,13 @@ int main(int argc, char **argv)
 {
     /* On recupere le nom du fichier JPEG sur la ligne de commande. */
     const char *filename = argv[1];
+
+
+    FILE *pf = fopen("test.jpeg", "wb");
+    fputc(0b10101010, pf);
+    fclose(pf);
     
+    // TEste bitstream    
     struct bitstream *stream = create_bitstream(filename);
     
     close_bitstream(stream);
