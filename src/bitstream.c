94b6bb5041e47f1d4865c7d00935f24cf1d2bb11
Mathieu Barbe <mat.barbe@gmail.com>
2017-05-31 15:20:30 +0200

    Ajout de l'ouverture du fichier

--- src/bitstream.c
+++ src/bitstream.c
@@ -1,15 +1,35 @@
 #include "bitstream.h"
 
 struct bitstream{
-                              char *filename;
+                              const char *filename;
+                              FILE *pfile;
+                              int32_t buffer;
+                              bool end_of_stream;
 };
 
 struct bitstream *create_bitstream(const char *filename){
+                              
                               struct bitstream *stream = malloc(sizeof(struct bitstream));
+                              stream->buffer = 0;
+                              stream->end_of_stream = false;
+                              stream->filename = filename;
+                              
+                              // Ouvertture du fichier
+                              stream->pfile = fopen(filename, "rb");
+                              	if (stream->pfile == NULL){
+                              	                              perror("File not found\n");
+                              	exit(EXIT_FAILURE);
+                              	 } // end if error 
+                              
+                              
+                              
                               return stream;
 } // end def
 
 void close_bitstream(struct bitstream *stream){
+                              
+                              //Fermetture du fichier
+                              if(fclose(stream->pfile)) perror("Erreur à la fermeture de l'image jpeg\n");
                               free(stream);
 } // end def
 
@@ -17,12 +37,13 @@ uint8_t read_bitstream(struct bitstream *stream,
                               uint8_t nb_bits,
                               uint32_t *dest,
                               bool discard_byte_stuffing){
-                              
+                              stream->buffer = 1;
                               return 0;
 } //end def
 
+// La fonction end_of_bitstream retourne true si le flux a été entièrement parcouru, false s’il reste des bits à lire. 
 bool end_of_bitstream(struct bitstream *stream){
-                              return false;
+                              return stream->end_of_stream;
 } // end def
 
 /* Optionnel! */
