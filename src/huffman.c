8be2c94d8138053004baa4a56bc482ab39772a16
Mathieu Barbe <mat.barbe@gmail.com>
2017-06-06 12:32:26 +0200

    erreur seg mais pas dans huffman

--- src/huffman.c
+++ src/huffman.c
@@ -23,10 +23,10 @@ struct node *create_node(bool sheet, uint8_t value){
 
 
 void free_tree(struct node *n){
-    //if(n->childs[0]) free_tree(n->childs[0]);
-    //if(n->childs[1]) free_tree(n->childs[1]);
+    if(n->childs[0]) free_tree(n->childs[0]);
+    if(n->childs[1]) free_tree(n->childs[1]);
     
-    //free(n);
+    free(n);
 } // end def
 
 extern struct huff_table *load_huffman_table(struct bitstream *stream,
@@ -172,7 +172,8 @@ uint8_t *nb_bits_read){
 
 extern void free_huffman_table(struct huff_table *table){
     fprintf(stderr, "debug : deep %d, tree %p\n", table->deep, table->tree);
-    //free_tree(table->tree);
-    //free(table);
+    free_tree(table->tree);
+    free(table);
+    fprintf(stderr, "Mémoire huffman free\n");
 } // end def
 
