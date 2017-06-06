ebdc2940d0eb77feb6934d015911b26a3e6103ef
Antoine He <Antoine.He@grenoble-inp.org>
2017-06-06 13:08:07 +0200

    Franchement ca avance

--- include/huffman.h
+++ include/huffman.h
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include "bitstream.h"
 
-struct huff_table{};
+struct huff_table;
 
 extern struct huff_table *load_huffman_table(struct bitstream *stream,
                                              uint16_t *nb_byte_read);
